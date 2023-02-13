541e46210d553b5c57d2e2f696d2ce66842837ec
Ben <benpontus@hotmail.co.uk>
2023-02-13 13:50:47 +0100

    Display RGB from fits

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ The lab computers can load the entire image in about 3 mins
 There is only one HDU (the primary one), we could add our masks to the same FITS file
 """
 
-
 src_path = "source_data/PROMISE-Q1-8micron-filled-v0_3.fits"
+
 with fits.open(src_path, memmap=True) as fits_data:
     fits_data.info()  # Shape is 12,000 x 120,000
     slice_data = fits_data["PRIMARY"].data[ Y_LOWER:Y_UPPER, X_LOWER:X_UPPER]
