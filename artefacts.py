c0d4bb6efdb2e25b69751beb0df1844a5f3b0df8
JoakimKiselman <89736416+JoakimKiselman@users.noreply.github.com>
2023-03-31 11:29:13 +0200

    added check for symmetry func

--- artefacts.py
+++ artefacts.py
@@ -3,9 +3,38 @@ from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
+from tqdm import tqdm
+
+def absolute_symmetry(value1, value2):
+    if abs(value1) == abs(value2):
+        return True
+    else:
+        return False
+
+def check_for_symmetry(peaksmax, peaksmin, margin_of_error):
+    center_index = None
+    min_start1 = 0
+    min_start2 = 1
+    symmetry = False
+    for i in peaksmax:
+        if i == 0:
+            center_index = i
+    
+    if center_index == None:
+        print("Center index = null")
+        return False
+    
+    for i in range(len.peaksmin):
+        if peaksmin[i] > 0 and peaksmin[min_start2] < 0:
+            min_start1 = i
+            exit
+        min_start2 += 1
+    if peaksmin[center_index+1] == peaksmin[center_index-1] and peaksmax[min_start1] ==peaksmax[min_start1]:
+        return True
+    
 
 full_data = False #Ändra till false om man inte vill dela upp hela datan
-file_path = "Q1-latest-whigal-85.fits"
+file_path = "C:/Users/joaki/Pictures/Q1-latest-whigal-85.fits"
 
 if full_data:
     data_list = []
@@ -37,8 +66,8 @@ size = 40
 datap = data[index[0], (index[1]-size):(index[1]+size)]
 X = np.linspace(0,2*size-1, num=2*size)
 
-peaksh, _ = find_peaks(-datap)
-peaksl, _ = find_peaks(datap)
+peaksh, _ = find_peaks(datap) #List of local maxima
+peaksl, _ = find_peaks(-datap) #List of local minima
 
 
 print(size-peaksl)
