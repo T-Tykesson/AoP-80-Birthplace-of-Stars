b4afa53da06367ddcffc9d0351a5e9374fd95244
T-Tykesson <75175408+T-Tykesson@users.noreply.github.com>
2023-05-01 17:28:13 +0200

    Småfix av funktioner och några buggar

--- artificial_cores.py
+++ artificial_cores.py
@@ -7,29 +7,34 @@ Created on Tue Mar 21 17:20:18 2023
 core test
 """
 import numpy as np
+import random
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
 
-def create_gaussian_filter(kernel_size, sigma=0.25, muu=0):
+def create_gaussian_filter(kernel_size, sigma=0.25, muu=(0, 0)):
     # Initializing value of x,y as grid of kernel size
     # in the range of kernel size
-    #sigma = np.random.uniform(0.25,0.05)
-    
-    #print(sigma)
-    
     x, y = np.meshgrid(np.linspace(-1, 1, kernel_size),
                        np.linspace(-1, 1, kernel_size))
-    dst = np.sqrt(x**2+y**2)
-    # lower normal part of gaussian
-    normal = 1/(2.0 * np.pi * sigma**2)
-    # Calculating Gaussian filter
-    gauss = np.exp(-((dst-muu)**2 / (2.0 * sigma**2))) * normal
-    #print(kernel_size, kernel_size//2)
-    #plt.plot(gauss[kernel_size//2+1, :])
-    #plt.title(f"Sigma:{sigma}, Kernel:{kernel_size}")
-    #plt.show()
-    return gauss
+    
+    dst = np.sqrt((x - muu[0])**2 + (y - muu[1])**2)
+    
+    # calculate the Gaussian values
+    normal = 1 / (2.0 * np.pi * sigma**2)
+    gauss = np.exp(-((dst)**2 / (2.0 * sigma**2))) * normal
+    center = kernel_size//2
+
+    #calculate hwhm and mass for inserted cores, note needs to be multiplied by intensity to get right values
+    indices = np.argwhere(gauss <= gauss[center, center]/2)
+    indices = indices[(indices != center).all(axis=1)]
+    closest_index = indices[np.argmin(np.linalg.norm(indices - center, axis=1))]
+
+    hwhm = round(np.linalg.norm(closest_index - center))
+    mask = gauss >= gauss[center, center]/2
+    mass = np.sum(gauss * mask)
+    
+    return gauss, hwhm, mass
 
 "Artificial star test, all same size"
 
@@ -73,73 +78,6 @@ def insert_circles_different_sizes(data, gaussian_list, nr, intensity="Random", 
     art_catalog = []
     data_copy = np.array(data, copy=True)
     
-    kernel_size = 10
-    gaussian_core = create_gaussian_filter(kernel_size)
-    h, w = gaussian_core.shape[:2]
-    circular_mask = create_circular_mask(h, w)
-    
-    art_core = gaussian_core.copy()
-    art_core[~circular_mask] = 0
-    
-    gaussian_core_2 = create_gaussian_filter(25)
-    h, w = gaussian_core_2.shape[:2]
-    circular_mask_2 = create_circular_mask(h, w)
-    
-    art_core_2 = gaussian_core_2.copy()
-    art_core_2[~circular_mask_2] = 0
-    
-    
-    fig = plt.figure(figsize=(13, 13), dpi=300)
-
-
-    plt.rcParams['axes.grid'] = False
-    plt.axis('off')
-    plt.tight_layout()
-    plt.subplots_adjust(left=0.1,
-            bottom=0.1,
-            right=0.9,
-            top=0.9,
-            wspace=0.3,
-            hspace=0.07)
-    plt.grid(None)
-
-    ax = fig.add_subplot(2, 2, 1)
-    im = ax.imshow(gaussian_core, cmap="hot")
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    fig.subplots_adjust(right=0.8)
-    fig.colorbar(im, cax=cax)
-    
-    ax = fig.add_subplot(2, 2, 2)
-    im = ax.imshow(art_core, cmap="hot")
-    #ax = plt.gca()
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    fig.subplots_adjust(right=0.8)
-    fig.colorbar(im, cax=cax)
-    #axs[0].imshow(slice, cmap="hot")
-    #axs[1].imshow(smoothed, cmap="hot")
-    #art_catalog_list.append(art_catalog)
-    
-    ax = fig.add_subplot(2, 2, 3)
-    im = ax.imshow(gaussian_core_2, cmap="hot")
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    fig.subplots_adjust(right=0.8)
-    fig.colorbar(im, cax=cax)
-    
-    ax = fig.add_subplot(2, 2, 4)
-    im = ax.imshow(art_core_2, cmap="hot")
-    #ax = plt.gca()
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    fig.subplots_adjust(right=0.8)
-    fig.colorbar(im, cax=cax)
-    plt.plot()
-    
-    
-    #axs[0].imshow(slice, cmap="hot")
-    #axs[1].imshow(smoothed, cmap="hot")
     for i in range(nr):
         size = len(gaussian_list[i])
         
@@ -152,11 +90,8 @@ def insert_circles_different_sizes(data, gaussian_list, nr, intensity="Random", 
             inten = intensity
 
         art_catalog.append([int(yrandom+size/2), int(xrandom+size/2), inten, size/2]) #want to get middle of point
-        
         data_copy[yrandom:yrandom+size,xrandom:xrandom+size] += inten*gaussian_list[i]
-    
-    #plot(data, dpi=300, colorbar=True, title="test", vmin=np.mean(data)-1*np.std(data), vmax=(np.mean(data)+6*np.std(data)), cmap="inferno")     
-    #plot(data_copy, dpi=300, colorbar=True, title="test", vmin=np.mean(data)-1*np.std(data), vmax=(np.mean(data)+6*np.std(data)), cmap="inferno")   
+
     return data_copy, art_catalog
 
 def insert_art_cores_data_slices(data_list, xslice, kernel_size=20, amount=1000, intensity="Random", int_min=50, int_max=350):
@@ -203,21 +138,28 @@ def insert_art_cores(data, kernel_size=20, amount=1000, intensity="Random", int_
 
 def insert_art_cores_2(data, size_min=5, size_max=130, amount=1000, intensity="Random", int_min=50, int_max=350):
     art_core_list=[]
+    radius_list = []
+    mass_list = []
     for i in range(amount):
-        kernel_size = np.random.randint(size_min, size_max)
-        gaussian_core = create_gaussian_filter(kernel_size)
+        kernel_size = random.randrange(size_min, size_max,2) #only odd values to get 1 middle value
+        gaussian_core, radius, mass = create_gaussian_filter(kernel_size)
         h, w = gaussian_core.shape[:2]
         circular_mask = create_circular_mask(h, w)
         
         art_core = gaussian_core.copy()
         art_core[~circular_mask] = 0
         art_core_list.append(art_core)
+        radius_list.append(radius)
+        mass_list.append(mass)
     
     art_data, art_catalog = insert_circles_different_sizes(data, art_core_list, amount, intensity=intensity, int_min=int_min, int_max=int_max)
     art_catalog = np.array(art_catalog)
     #art_catalog_list.append(art_catalog)
     
-
+    #potential plotting, note needs to be multiplied by random intensity value
+    #radius_list = np.array(radius_list, dtype=float)
+    #mass_list = np.array(mass_list, dtype=float)
+    #plotting.scatter_plot(radius_list*0.02, mass_list*0.0081, xlabel="radie [pc]", ylabel="massa [M$_{\odot}$]", yscale="log", xscale='log')
 
     art_catalog_tuples = list(map(tuple, art_catalog[:,0:2]))
     
